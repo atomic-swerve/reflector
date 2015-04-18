7fadfcab418104694edf26537db63c4c89913da2
atomic-swerve <lppsmanabeast@gmail.com>
2015-04-17 20:52:09 -0700

    Reorg'd project slightly, added initial scan behaviour and gun animation

---
+++ Assets/Scripts/ScanBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScanBehaviour : MonoBehaviour {
+
+	private Transform gun;
+
+	void Start() {
+		gun = GetComponentInChildren<Camera>().transform.FindChild("Gun").transform;
+		StartCoroutine(ScanInput());
+	}
+
+	IEnumerator ScanInput() {
+		while (true) {
+			if (Input.GetAxis("Fire2") > 0f) {
+				yield return StartCoroutine(Scan ());
+			}
+			yield return null;
+		}
+	}
+
+	IEnumerator Scan() {
+		while (true) {
+			if (Input.GetAxis("Fire2") < 1) {
+				break;
+			}
+			Debug.DrawRay(gun.position, gun.forward * 50, Color.green);
+			Physics.Raycast(gun.position, gun.forward, 50);
+
+			yield return null;
+		}
+	}
+}
