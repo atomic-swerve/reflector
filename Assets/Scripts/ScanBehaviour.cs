b5668afbdd091b33686b0ab666d00935e83bed1c
atomic-swerve <lppsmanabeast@gmail.com>
2015-04-18 11:16:44 -0700

    Added trail as line, detecting if hitting object

--- Assets/Scripts/ScanBehaviour.cs
+++ Assets/Scripts/ScanBehaviour.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class ScanBehaviour : MonoBehaviour {
 
+	public float scanLength = 10f;
 	public LayerMask layerMask;
 
 	private Transform gunT;
 	private Animator gunA;
 	private Transform camT;
+	private Transform laserT;
+	private LineRenderer trail;
 
 	private RaycastHit ray;
 
@@ -15,6 +18,9 @@ public class ScanBehaviour : MonoBehaviour {
 		gunT = GetComponentInChildren<Camera>().transform.FindChild("Gun").transform;
 		gunA = GetComponentInChildren<Camera>().transform.FindChild("Gun").GetComponent<Animator>();
 		camT = GetComponentInChildren<Camera>().transform;
+		laserT = GetComponentInChildren<Camera>().transform.Find("Gun/Mesh/LaserPoint").transform;
+		trail = GetComponentInChildren<LineRenderer>();
+		gunA.Play ("GunReturn");
 		StartCoroutine(ScanInput());
 	}
 
@@ -30,23 +36,36 @@ public class ScanBehaviour : MonoBehaviour {
 	IEnumerator Scan() {
 		gunA.StopPlayback();
 		gunA.Play("GunCenter");
+		trail.enabled = true;
+		trail.SetColors(new Color(.7f,0,0,.3f),new Color(.7f,0,0,.2f));
 		while (true) {
 			if (Input.GetAxis("Fire2") < 1) {
 				gunA.StopPlayback();
 				gunA.Play("GunReturn");
 				gunT.localRotation = Quaternion.identity;
+				trail.enabled = false;
 				break;
 			}
 
-			if (Physics.Raycast(camT.position, camT.forward, out ray, 10, layerMask)) {
-				Debug.DrawRay(camT.position, camT.forward * Vector3.Distance(ray.point,camT.position), Color.green);
-				gunT.LookAt(ray.point);
-				Debug.DrawRay(gunT.position, gunT.forward * Vector3.Distance(ray.point,gunT.position), Color.blue);
+			trail.SetPosition(0,laserT.position);
+
+			if (Physics.Raycast(camT.position, camT.forward, out ray, scanLength, layerMask)) {
+				gunT.rotation = Quaternion.RotateTowards(gunT.rotation, Quaternion.LookRotation((ray.point - gunT.position).normalized), 5f);
+				trail.SetPosition(1,ray.point);
+				trail.SetColors(new Color(.7f,0,0,.6f),new Color(.7f,0,0,.5f));
 			} else {
-				Debug.DrawRay(camT.position, camT.forward * 10, Color.red);
+				gunT.localRotation = Quaternion.RotateTowards(gunT.localRotation, Quaternion.identity, 5f);
+				trail.SetPosition(1, laserT.position + laserT.up * scanLength);
+				trail.SetColors(new Color(.7f,0,0,.3f),new Color(.7f,0,0,.2f));
 			}
 
 			yield return null;
 		}
 	}
+
+	/*IEnumerator LaserFlicker() {
+		while (true) {
+			yield return new WaitForSeconds;
+		}
+	}*/
 }
