60041b4fe41e776110b60941ffe20b4d7b333649
atomic-swerve <lppsmanabeast@gmail.com>
2015-04-19 11:53:38 -0700

    Refactored Scannable behaviour

--- Assets/Scripts/ScanBehaviour.cs
+++ Assets/Scripts/ScanBehaviour.cs
@@ -19,6 +19,11 @@ public class ScanBehaviour : MonoBehaviour {
 	private Transform laserT;
 	private LineRenderer trail;
 
+	private Scannable scannable;
+	private bool scanning = false;
+	private bool finishedScan = false;
+	private float timeToScan = 0f;
+
 	private RaycastHit ray;
 
 	void Start() {
@@ -33,49 +38,71 @@ public class ScanBehaviour : MonoBehaviour {
 
 	IEnumerator ScanInput() {
 		while (true) {
-			if (Input.GetAxis(programmingInput) > 0f) {
-				yield return StartCoroutine(Scan(programmingInput, programmingColour));
-			}
-			if (Input.GetAxis(scanningInput) > 0f) {
-				yield return StartCoroutine(Scan(scanningInput, scanningColour));
+			if (!finishedScan) {
+				if (Input.GetAxis(programmingInput) > 0f) {
+					yield return StartCoroutine(Scan(programmingInput, programmingColour, true));
+				}
+				if (Input.GetAxis(scanningInput) > 0f) {
+					yield return StartCoroutine(Scan(scanningInput, scanningColour, false));
+				}
 			}
 			yield return null;
 		}
 	}
 
-	IEnumerator Scan(string programming, Color c) {
+	IEnumerator Scan(string programming, Color c, bool readOrWrite) {
 		gunA.StopPlayback();
 		gunA.Play("GunCenter");
 		trail.enabled = true;
 		trail.SetColors(c,c);
 		while (true) {
 			if (Input.GetAxis(programming) < 1) {
+				StopCoroutine("BeginReading");
+				scanning = false;
 				gunA.StopPlayback();
 				gunA.Play("GunReturn");
 				gunT.localRotation = Quaternion.identity;
 				trail.enabled = false;
+				finishedScan = false;
 				break;
 			}
-			trail.
-				SetPosition(0,laserT.position);
+			trail.SetPosition(0,laserT.position);
 			
-			if (Physics.Raycast(camT.position, camT.forward, out ray, scanLength, layerMask)) {
+			if (!finishedScan && Physics.Raycast(camT.position, camT.forward, out ray, scanLength, layerMask)) {
 				gunT.rotation = Quaternion.RotateTowards(gunT.rotation, Quaternion.LookRotation((ray.point - gunT.position).normalized), 5f);
-				trail.SetPosition(1,ray.point);
-				trail.SetColors(c,c);
+				scannable = ray.transform.GetComponent<Scannable>();
+				if (scannable != null) {
+					SetLaser(c, ray.point);
+					if (!scanning) {
+						StartCoroutine(BeginReading(scannable, readOrWrite));
+					}
+				} else {
+					SetLaser(missingColour, ray.point);
+				}
 			} else {
+				StopCoroutine("BeginReading");
+				scanning = false;
 				gunT.localRotation = Quaternion.RotateTowards(gunT.localRotation, Quaternion.identity, 5f);
-				trail.SetPosition(1, laserT.position + laserT.up * scanLength);
-				trail.SetColors(missingColour,missingColour);
+				SetLaser(missingColour, laserT.position + laserT.up * scanLength);
 			}
 
 			yield return null;
 		}
 	}
 
-	/*IEnumerator LaserFlicker() {
-		while (true) {
-			yield return new WaitForSeconds;
-		}
-	}*/
+	void SetLaser(Color c, Vector3 end) {
+		trail.SetPosition(1,end);
+		trail.SetColors(c,c);
+	}
+
+	IEnumerator BeginReading(Scannable scannable, bool readOrWrite) {
+		print ("Begin reading...");
+		scanning = true;
+		timeToScan = scannable.TimeToScan(readOrWrite); 
+		yield return new WaitForSeconds(timeToScan);
+		print ("Finished reading!");
+		scanning = false;
+		finishedScan = true;
+		trail.enabled = false;
+	}
 }
--- Assets/Scripts/Scannable.cs
+++ Assets/Scripts/Scannable.cs
@@ -3,5 +3,31 @@ using System.Collections;
 
 public class Scannable : MonoBehaviour {
 
+	public float timeToRead = .2f;
+	public float timeToWrite = .5f;
+	public Program[] methods;
 
+	public float TimeToScan (bool readOrWrite)
+	{
+		if (readOrWrite) {
+			return timeToRead;
+		} else {
+			return timeToWrite;
+		}
+	}
+
+	public Program[] DoneScanning (bool readOrWrite, int programIndex = 0) {
+		if (readOrWrite) {
+			return methods;
+		} else {
+			return null;
+		}
+	}
+
+}
+
+public class Program {
+	public string programName = "Default";
+	public string programDescription = "Default Description";
+	public Texture2D programIcon;
 }
